8887505cb9f090d7558680db8e2b38e9225a7e31
chuzksy-codeactive <chuzksy@gmail.com>
2022-06-08 04:34:40 +0100

    export db connection to all module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(
+        private prismaService: PrismaService
+    ) {
+
+    }
+
     login() { 
         return { msg: 'I am signed in' };
     }
