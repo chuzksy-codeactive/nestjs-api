d2e6a4aba9eb4cfe15847b14abccee0208b4d681
chuzksy-codeactive <chuzksy@gmail.com>
2022-06-09 04:44:10 +0100

    Add user to the db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { prisma } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable({})
 export class AuthService {
@@ -9,10 +13,43 @@ export class AuthService {
 
     }
 
-    login() { 
-        return { msg: 'I am signed in' };
+    async login(dto: AuthDto) { 
+        console.log(dto);
+        const user = await this.prismaService.user.findFirst({
+            where: {
+                email: dto.email
+            },
+        });
+
+        if (!user)
+            throw new ForbiddenException("Credential incorrect");
+        
+        const pwdMatches = await argon.verify(user.hash, dto.password);
+        if (!pwdMatches)
+            throw new ForbiddenException("Credentials incorrect");
+        
+        delete user.hash;
+        return user;
     }
-    signup() { 
-        return { mgs: 'I am signed up' };
+    async signup(dto: AuthDto) { 
+        try {
+            const hash = await argon.hash(dto.password);
+            const user = await this.prismaService.user.create({
+              data: {
+                email: dto.email,
+                hash,
+              },
+            });
+            delete user.hash;
+
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    throw new ForbiddenException("Credentials taken");
+                }
+            }
+            throw error;
+        }
     }
  }
